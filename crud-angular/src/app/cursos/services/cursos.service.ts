52e1cae53bfcd040bd3f975fcf786f1e639382a2
Jose Antonio <jose01.11.93@gmail.com>
2021-10-27 17:41:17 -0300

    Criação do serviço de cursos

---
+++ crud-angular/src/app/cursos/services/cursos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Curso } from '../model/curso';
+import { delay, first, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  private readonly API = '/assets/cursos.json';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  listAll() {
+    return this.httpClient.get<Curso[]>(this.API)
+      .pipe(
+        first(),
+        tap(cursos => console.log(cursos))
+      );
+  }
+}
