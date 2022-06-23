7c6991046f39073819041248b104a3b3c90d81f8
Jose <jose01.11.93@gmail.com>
2022-06-22 23:37:05 -0300

    atualização para Angular 13

---
+++ crud-spring/src/main/java/com/jkconsultoria/controler/CourseController.java
@@ -0,0 +1,25 @@
+package com.jkconsultoria.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jkconsultoria.model.Course;
+import com.jkconsultoria.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+    
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() { 
+        return courseRepository.findAll();
+    }
+}
