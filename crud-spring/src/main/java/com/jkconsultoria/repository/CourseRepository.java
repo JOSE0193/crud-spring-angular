8e69b3226a8c49de958589fbec2ebd2734b47b2e
JOSE0193 <jose01.11.93@gmail.com>
2024-09-12 14:05:02 -0300

    divisão do repositório

--- crud-spring/src/main/java/com/jkconsultoria/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jkconsultoria.repository;
-
-import com.jkconsultoria.model.Course;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    
-}
---
+++ src/app/shared/app-material.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+
+@NgModule({
+  exports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatSnackBarModule,
+  ],
+  declarations: [],
+})
+export class AppMaterialModule {}
